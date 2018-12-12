46531c27df0f9385be1d388756eb14911c9137d7
Marcus Plenty <marcusplenty@gmail.com>
2018-12-12 18:10:05 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,6 @@
+class Post
+  attr_accessor :name, :genre, :title, :author
+  def author
+    return @author
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,6 @@
+class Song
+  attr_accessor :name, :title, :artist
+  def artists
+    return @author
+  end
+end
